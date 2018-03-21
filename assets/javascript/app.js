45cfcc707c145d5586a65ffc7ce7f668a88ccd1b
Isaac <isaaclubanko@gmail.com>
2018-03-21 18:53:24 -0400

    added google maps functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@
 //FozN4hA9DAjuptuej3UXIG5BT0mmLcrTx_LQEFyRUxSmJuGYe5Zic3os-8GGQPiv0TSfvxz6lkfxmyEvM0st4OFKA3kJE5bEMGQBpL8NuOlW-WXAWsOLO5ykfVCwWnYx
 
 //UGLY URL VARIABLES//
-//var openWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&APPID=beeeb0200ae49646011f7917db233044";
+//var openWeatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=beeeb0200ae49646011f7917db233044";
 var googleMapsTimeZoneQueryURL = "https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyACzWLfVVhDTJc2ivhNQio_LjbUrLfYViU";
 var googleCustomSearchAPIKey = "AIzaSyAod0vUH_7hewxjW_4HPxbZbeB0TzEbPus";
 var googleCustomSearchURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAod0vUH_7hewxjW_4HPxbZbeB0TzEbPus&cx=017576662512468239146:omuauf_lfve&q=lectures";
@@ -13,7 +13,7 @@ var googleCustomSearchURL = "https://www.googleapis.com/customsearch/v1?key=AIza
 
 //actual variables
 
-var City = "Rome";
+var city = "Rome, Italy";
 var results = undefined;
 var coord = {lat: 0 , lng: 0 };
 
@@ -24,7 +24,7 @@ var coord = {lat: 0 , lng: 0 };
 function initMap(coord) {
     var uluru = coord;
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 9,
       center: uluru
     });
     var marker = new google.maps.Marker({
@@ -36,28 +36,41 @@ function initMap(coord) {
 $(document).ready(function(){
 
 
-$.ajax({
-    url:"https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=imperial&APPID=beeeb0200ae49646011f7917db233044",
-    method: "get"
-}).then(function(response){
-    results = response;
-    console.log(response);
-    console.log(response.name);
-    console.log(response.main.temp);
-    coord.lat = response.coord.lat;
-    coord.lng = response.coord.lon;
-    //coords
-    initMap(coord);
-    $("#weather").html("<p>" + response.main.temp + "</p>")
-});
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDZvgtMtB6eoQymC3arARneZq557FGTsC0",
+        authDomain: "travelproj-2dc6d.firebaseapp.com",
+        databaseURL: "https://travelproj-2dc6d.firebaseio.com",
+        projectId: "travelproj-2dc6d",
+        storageBucket: "travelproj-2dc6d.appspot.com",
+        messagingSenderId: "625003623246"
+    };
+    firebase.initializeApp(config);
 
-$.ajax({
-    url:googleMapsTimeZoneQueryURL,
-    method: "get"
-}).then(function(response){
-    results = response;
-    console.log(response);
-});
 
-    //initMap(coord);
+    $("#destination").on("click", function(){
+        $.ajax({
+            url:"https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=beeeb0200ae49646011f7917db233044",
+            method: "get"
+        }).then(function(response){
+            results = response;
+            console.log(response);
+            console.log(response.name);
+            console.log(response.main.temp);
+            coord.lat = response.coord.lat;
+            coord.lng = response.coord.lon;
+            //coords
+            initMap(coord);
+            $("#weather").html("<p>" + response.main.temp + "</p>")
+        });
+
+        $.ajax({
+            url:googleMapsTimeZoneQueryURL,
+            method: "get"
+        }).then(function(response){
+            results = response;
+            console.log(response);
+        });
+    });
+
 });
