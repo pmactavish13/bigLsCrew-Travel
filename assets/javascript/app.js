13f3777e465b37f5802aa64140d73f6a3cb0d071
Isaac <isaaclubanko@gmail.com>
2018-03-28 18:21:06 -0400

    final version for now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,31 @@
 var destination;
 var city;
 var country;
-
-
+var cityLower;
+var numSearchedCounter;
+var recentSearches = 0;
+var cityExists = false
 var numSearched = 1
 var results = undefined;
 var coord = { lat: 0, lng: 0 };
 var dateAndTime = "";
 
+
+//FUNCTIONS
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBNsIe37RELMNqecsYHHWmmhcw59jQkf88",
+    authDomain: "practicaltravel-63ff1.firebaseapp.com",
+    databaseURL: "https://practicaltravel-63ff1.firebaseio.com",
+    projectId: "practicaltravel-63ff1",
+    storageBucket: "practicaltravel-63ff1.appspot.com",
+    messagingSenderId: "600055377899"
+};
+
+// set a reference to the database service
+firebase.initializeApp(config);
+var database = firebase.database();
 
 //FUNCTIONS
 
@@ -110,19 +128,19 @@ function googleSearchRest(){
     cityPlaceApi = $("#destination").val().trim();
     console.log($("#destination").val().trim())
     $.ajax({
-        url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + cityPlaceApi + "&key=AIzaSyBHOtkHIyowW6axP4vdTXKYOKGpv2k2IM8",
+        url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + cityPlaceApi + "&key=AIzaSyA-g_fVJ6QUtrNDHHh0RpCNw3Un5wX8sc4",
         method: "get"
     }).then(function (data) {
         results = data;
         console.log(data);
-        for(var i=0; i<4; i++){
-            var restaurant11=data.results[i].name;
-            var adress11=data.results[i].formatted_address;
+        /*for(var i=0; i<4; i++){
+            //var restaurant11=data.results[i].name;
+            //var adress11=data.results[i].formatted_address;
             //  $("#rest-address").append("<div>" + restaurant11 + "</div>");
             //  $("#rest-address").append("<div>" + address11 + "</div>");
             console.log(restaurant11);
             console.log(adress11);
-        }
+        }*/
          var $restaurant1 = data.results[0].name;
          var $address1 = data.results[0].formatted_address;
          //$restaurant1.addClass('rest-name-class');
@@ -173,6 +191,99 @@ function googleSearchHealth(){
         //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
     });
 }
+// DATABASE FUNCTION
+function travelDatabase() {
+    // Capture User Inputs and store them into variables
+    var destination = $("#destination").val().trim();
+    var destArr = destination.split(",");
+    city = destArr[0].trim();
+    country = destArr[1].trim();
+    cityLower = city.toLowerCase();
+    database.ref('travelPlans').limitToFirst(1).once('value', function (snapshot) {
+        // if data exists
+        if (snapshot.exists()) {
+            ifExists();;
+        } else {
+            updateDatabase();
+        }
+    });
+
+};
+
+// DATABASE FUNCTION
+// Check to see if city exists
+function ifExists() {
+    var cityRef = firebase.database().ref('travelPlans');
+    cityRef.once('value', function (citySnapshot) {
+        var myJSON = JSON.stringify(citySnapshot);
+        var entrySearch = JSON.parse(myJSON);
+        var length = (Object.keys(entrySearch).length);
+        var entriesSearched = 1
+        citySnapshot.forEach(function (snapshot) {
+            cityExists = false;
+            if (snapshot.val().city == cityLower) {
+                cityExists = true
+                var myJSON = JSON.stringify(snapshot);
+                var citySearch = JSON.parse(myJSON);
+                var dataKey = snapshot.key;
+                numSearchedCounter = (citySearch.numSearched);
+                numSearchedCounter++;
+                firebase.database().ref("travelPlans")
+                    .child(dataKey).child('numSearched')
+                    .set(numSearchedCounter);
+            }
+            if ((entriesSearched == length) && (cityExists == false)) {
+                updateDatabase()
+            }
+            if ((entriesSearched == length) && (cityExists == true)) {
+                var cityExists = 'false'
+                updateScreen()
+            }
+            if ((entriesSearched !== length) && (cityExists == true)) {
+                entriesSearched++
+            } else {
+                entriesSearched++
+            }
+        });
+    });
+}
+
+// DATABASE FUNCTION
+// add new search parameters to database
+function updateDatabase() {
+    database.ref('travelPlans').push({
+        dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        city: cityLower,
+        country: country,
+        numSearched: 1,
+    });
+    updateScreen();
+};
+
+// DATABASE FUNCTION
+//update the screen - clear search parameters, update recent and most popular
+function updateScreen() {
+    // Clear the input boxes on the screen and replace with placer text
+    $("#destination").val("City, Country");
+    $("#popular-searches").empty();
+    // Update popular and recent searches
+
+    recentSearches++;
+    var recentCity = "<h5 class='recent-city'>" + city + "</h5>";
+    if (recentSearches > 3) {
+        $(".recent-city h5:last-child").remove()
+    }
+    $("#recent-searches").prepend(recentCity);
+
+    var numRef = firebase.database().ref('travelPlans');
+    //numRef.once('value', function (numSnapshot) {
+    //var ref = db.ref("dinosaurs");
+    numRef.orderByChild("numSearched").limitToLast(3).on("child_added", function (snapshot) {
+        var popular = snapshot.val().city
+        var popularCity = "<h5 class='popular-city'>" + popular + "</h5>";
+        $("#popular-searches").append(popularCity);
+    });
+}
 
 
 
@@ -195,21 +306,28 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     */
 
-    $('#submit-data').on('click', function (event) {
-        event.preventDefault();
-        
-        //OPEN WEATHER API
-        tempAndCoord();           
+   $('#submit-data').on('click', function (event) {
+    event.preventDefault();
 
-        //GOOGLE SEARCH API CALL
-        googleSearchRest();
-        //GOOGLE HEALTH SEARCH API CALL
-        googleSearchHealth();
+    //OPEN WEATHER API
+    tempAndCoord();
 
-        //gettySearch();
+    //GOOGLE SEARCH API CALL
+    googleSearchRest();
+    //GOOGLE HEALTH SEARCH API CALL
+    googleSearchHealth();
 
+    //FIREBASE 
+    travelDatabase();
+    // reset search counters for each new input
+    numSearched = 1;
+    numSearchedCounter = 1;
 
-    });
+    // Flickr with slidesjs
+    flickrSlideshow();
+
+});
+
     
 });
 
