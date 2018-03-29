537919b4add25fa03f3ee6f1b0676a348299f6b3
Paula MacTavish <perilousp@hotmail.com>
2018-03-29 18:44:44 -0400

    fixing database glitch, uppercase first letter in popular cities, remove last child so only 3 show in recent searches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,33 +24,39 @@ var config = {
     messagingSenderId: "600055377899"
 };
 
-// set a reference to the database service
+// set a reference to the database service load recent searches to screen
 firebase.initializeApp(config);
 var database = firebase.database();
+database.ref('travelPlans').limitToFirst(1).once('value', function (snapshot) {
+    // if data exists
+    if (snapshot.exists()) {
+        updatePopularSearches();
+    }
+});
 
 //FUNCTIONS
 
 //Weather and Coordinates
 
-function tempAndCoord(){
+function tempAndCoord() {
     destination = $("#destination").val().trim();
     //make sure there's a comma in there
-    if(destination.includes(",") === false){
+    if (destination.includes(",") === false) {
         alertify.error("wrong format");
     }
     var destArr = destination.split(",");
     city = destArr[0].trim();
     country = destArr[1].trim();
     $.ajax({
-        url:"https://api.openweathermap.org/data/2.5/weather?q=" + destination + "&units=imperial&APPID=beeeb0200ae49646011f7917db233044",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + destination + "&units=imperial&APPID=beeeb0200ae49646011f7917db233044",
         method: "get"
-    }).then(function(response){
+    }).then(function (response) {
         results = response;
-        console.log(response);
-        console.log(response.name);
-        console.log("temp: " + response.main.temp + "degrees");
-        console.log("humidity: " + response.main.humidity + " %");
-        console.log("weather: " + response.weather[0].description);
+        //    console.log(response);
+        //    console.log(response.name);
+        //    console.log("temp: " + response.main.temp + "degrees");
+        //    console.log("humidity: " + response.main.humidity + " %");
+        //    console.log("weather: " + response.weather[0].description);
         coord.lat = response.coord.lat;
         coord.lng = response.coord.lon;
         //coords
@@ -75,64 +81,109 @@ function initMap(coord) {
         position: uluru,
         map: map
     });
-    
-    console.log(map);
+
+    //    console.log(map);
 }
 
 
 //GETTY IMAGES SEARCH FUNCTION 
 
-function gettySearch() {
-    var apiKey = 'apiKey';
-    $.ajax(
-        {
-            type: 'GET',
-            url: "https://api.gettyimages.com/v3/search/images/creative?phrase=" + destination,
-            beforeSend: function (request) {
-                request.setRequestHeader("Api-Key", apiKey);
-            }
-        })
-        .done(function (data) {
-            console.log("Success with data")
-            for (var i = 0; i < data.images.length; i++) {
-                $(".slideshow").append("<img src='" + data.images[i].display_sizes[0].uri + "'/>");
-            }
-        })
-        .fail(function (data) {
-            alert(JSON.stringify(data, 2))
+function flickrSlideshow() {
+    $(function () {
+        $("#photo1").empty;
+        $("#photo2").empty
+        //get JSON
+        var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+        $.getJSON(flickrAPI, {
+            tags: "architecture, " + city + " ,sights",
+            tagmode: "all",
+            format: "json",
+            extras: "url_l"
+        }).done(function (data) {
+            //console.log(data);
+            $.each(data.items, function (index, item) {
+                //    console.log(item);
+                $("<img>").attr("src", item.media.m + "/350x380").appendTo("#slides");
+                if (index == 9) {
+                    return false;
+                }
+            });
+
+        }).fail(function () {
+            alert("Ajax call failed.");
         });
-    return false;
-}
+    })
+    $(function () {
+        //get JSON
+        var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+        $.getJSON(flickrAPI, {
+            tags: "architecture, " + city + " ,sights",
+            tagmode: "all",
+            format: "json",
+            extras: "url_l"
+        }).done(function (data) {
+            // console.log(data);
+            for (var i = 2; i = 0; i--) {
+                data.items, function (index, item) {
+                    //        console.log(item);
+                    $("<img>").attr("src", item[i].media.m + "/350x380").appendTo("#photo[i]");
+                    if (index == 1) {
+                        return false;
+                    }
+                }
+            };
+        }).fail(function () {
+            alert("Ajax call failed.");
+        });
+    })
+};
+
+function slidesJS() {
+    $(function () {
+        $("#slides").slidesjs({
+            width: 380,
+            height: 350,
+            play: {
+                active: true,
+                auto: true,
+                interval: 4000,
+                swap: true
+            },
+        });
+    });
+};
 
 //TIMEZONE DB FUNCTIONf
-    function timeZone() {
-        $.ajax({
-            url: "https://api.timezonedb.com/v2/get-time-zone?key=GDN58Z4BNM7G&format=json&by=position&lat=" + coord.lat + "&lng=" + coord.lng,
-            method: "get"
-        }).then(function (response) {
-            results = response;
-            console.log(response);
-            //v2/resources/media/console.log(response);
-            var foreignTime = moment(response.formatted);
-            console.log(foreignTime.format("HH:mm MM/DD/YYYY"));
-            console.log(foreignTime.format("hh:mm a MM/DD/YYYY"));
-            dateAndTime = foreignTime.format("HH:mm  MM/DD/YYYY ");
-            var standard;
-            $("#timezone").text(dateAndTime);
-        });
-    }
+function timeZone() {
+    $.ajax({
+        url: "https://api.timezonedb.com/v2/get-time-zone?key=GDN58Z4BNM7G&format=json&by=position&lat=" + coord.lat + "&lng=" + coord.lng,
+        method: "get"
+    }).then(function (response) {
+        results = response;
+        // console.log(response);
+        //v2/resources/media/console.log(response);
+        var foreignTime = moment(response.formatted);
+        // console.log(foreignTime.format("HH:mm MM/DD/YYYY"));
+        // console.log(foreignTime.format("hh:mm a MM/DD/YYYY"));
+        dateAndTime = foreignTime.format("HH:mm   MM/DD/YYYY ");
+        var standard;
+        $("#timezone").text(dateAndTime);
+    });
+}
 
 //GOOGLE SEARCH RESTAURANT FUNCTION
 
-function googleSearchRest(){
+function googleSearchRest() {
     cityPlaceApi = $("#destination").val().trim();
-    console.log($("#destination").val().trim())
+    // console.log($("#destination").val().trim())
     $.ajax({
         url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" + cityPlaceApi + "&key=AIzaSyA-g_fVJ6QUtrNDHHh0RpCNw3Un5wX8sc4",
         method: "get"
     }).then(function (data) {
         results = data;
-        console.log(data);
+        // console.log(data);
         /*for(var i=0; i<4; i++){
             //var restaurant11=data.results[i].name;
             //var adress11=data.results[i].formatted_address;
@@ -141,56 +192,57 @@ function googleSearchRest(){
             console.log(restaurant11);
             console.log(adress11);
         }*/
-         var $restaurant1 = data.results[0].name;
-         var $address1 = data.results[0].formatted_address;
-         //$restaurant1.addClass('rest-name-class');
-         //address1.addClass('rest-address-class');
-         $("#rest-name1").text($restaurant1  );
-         $("#rest-address1").text($address1 );
-         //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
-       
-          var restaurant2 = data.results[1].name;
-          var address2 = data.results[1].formatted_address;
-         // restaurant2.addClass('rest-name-class');
-        // address2.addClass('rest-address-class');
-          $("#rest-name2").text( restaurant2 );
-          $("#rest-address2").text(address2 );
-
-          var restaurant3 = data.results[2].name;
-          var address3 = data.results[2].formatted_address;
-         // restaurant3.addClass('rest-name-class');
-         //address3.addClass('rest-address-class');
-         $("#rest-name3").text( restaurant3 )
-         $("#rest-address3").text(address3 )
-         //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
-
-         var restaurant4 = data.results[3].name;
-         var address4 = data.results[3].formatted_address;
-         //restaurant4.addClass('rest-name-class');
-        //address4.addClass('rest-address-class');
-        $("#rest-name4").text( restaurant4 )
-        $("#rest-address4").text( address4 )
-        console.log(restaurant4);
+        var $restaurant1 = data.results[0].name;
+        var $address1 = data.results[0].formatted_address;
+        //$restaurant1.addClass('rest-name-class');
+        //address1.addClass('rest-address-class');
+        $("#rest-name1").text($restaurant1);
+        $("#rest-address1").text($address1);
         //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
+
+        var restaurant2 = data.results[1].name;
+        var address2 = data.results[1].formatted_address;
+        // restaurant2.addClass('rest-name-class');
+        // address2.addClass('rest-address-class');
+        $("#rest-name2").text(restaurant2);
+        $("#rest-address2").text(address2);
+
+        var restaurant3 = data.results[2].name;
+        var address3 = data.results[2].formatted_address;
+        // restaurant3.addClass('rest-name-class');
+        //address3.addClass('rest-address-class');
+        $("#rest-name3").text(restaurant3)
+        $("#rest-address3").text(address3)
+        //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
+
+        var restaurant4 = data.results[3].name;
+        var address4 = data.results[3].formatted_address;
+        //restaurant4.addClass('rest-name-class');
+        //address4.addClass('rest-address-class');
+        $("#rest-name4").text(restaurant4)
+        $("#rest-address4").text(address4)
+        // console.log(restaurant4);
+        // $("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
     });
 }
 
 //GOOGLE SEARCH HEALTH FUNCTION 
 
-function googleSearchHealth(){
+function googleSearchHealth() {
     cityHealthApi = $("#destination").val().trim();
-    console.log($("#destination").val().trim())
+    // console.log($("#destination").val().trim())
     $.ajax({
         url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=health+news+in+" + cityHealthApi + "&key=AIzaSyBHOtkHIyowW6axP4vdTXKYOKGpv2k2IM8",
         method: "get"
     }).then(function (responseCityHealth) {
         results = responseCityHealth;
-        console.log(responseCityHealth);
+        // console.log(responseCityHealth);
         //$("#restaurant").html("<div>" + response.results[0].name + "</div>")
         //$("#restaurant").append("<div>" + response.results[0].formatted_address + "</div>")
         //$("#restaurant").append("<div>" + response.results[0].photos[0].photo_reference + "</div>")
     });
 }
+
 // DATABASE FUNCTION
 function travelDatabase() {
     // Capture User Inputs and store them into variables
@@ -202,12 +254,12 @@ function travelDatabase() {
     database.ref('travelPlans').limitToFirst(1).once('value', function (snapshot) {
         // if data exists
         if (snapshot.exists()) {
-            ifExists();;
+            ifExists();
+            updatePopularSearches();
         } else {
             updateDatabase();
         }
     });
-
 };
 
 // DATABASE FUNCTION
@@ -218,11 +270,11 @@ function ifExists() {
         var myJSON = JSON.stringify(citySnapshot);
         var entrySearch = JSON.parse(myJSON);
         var length = (Object.keys(entrySearch).length);
-        var entriesSearched = 1
+        var entriesSearched = 1;
+        cityExists = false;
         citySnapshot.forEach(function (snapshot) {
-            cityExists = false;
             if (snapshot.val().city == cityLower) {
-                cityExists = true
+                cityExists = true;
                 var myJSON = JSON.stringify(snapshot);
                 var citySearch = JSON.parse(myJSON);
                 var dataKey = snapshot.key;
@@ -231,19 +283,17 @@ function ifExists() {
                 firebase.database().ref("travelPlans")
                     .child(dataKey).child('numSearched')
                     .set(numSearchedCounter);
-            }
+                updateScreen();
+            };
             if ((entriesSearched == length) && (cityExists == false)) {
-                updateDatabase()
-            }
-            if ((entriesSearched == length) && (cityExists == true)) {
-                var cityExists = 'false'
-                updateScreen()
-            }
+                updateDatabase();
+            };
             if ((entriesSearched !== length) && (cityExists == true)) {
-                entriesSearched++
-            } else {
-                entriesSearched++
-            }
+                entriesSearched++;
+            };
+            if ((entriesSearched !== length) && (cityExists == false)) {
+                entriesSearched++;
+            };
         });
     });
 }
@@ -267,68 +317,60 @@ function updateScreen() {
     $("#destination").val("City, Country");
     $("#popular-searches").empty();
     // Update popular and recent searches
-
-    recentSearches++;
-    var recentCity = "<h5 class='recent-city'>" + city + "</h5>";
-    if (recentSearches > 3) {
-        $(".recent-city h5:last-child").remove()
-    }
-    $("#recent-searches").prepend(recentCity);
-
-    var numRef = firebase.database().ref('travelPlans');
-    //numRef.once('value', function (numSnapshot) {
-    //var ref = db.ref("dinosaurs");
-    numRef.orderByChild("numSearched").limitToLast(3).on("child_added", function (snapshot) {
-        var popular = snapshot.val().city
-        var popularCity = "<h5 class='popular-city'>" + popular + "</h5>";
-        $("#popular-searches").append(popularCity);
-    });
+    updateRecentSearches();
+    updatePopularSearches();
 }
 
+function updateRecentSearches() {
+    recentSearches++;
+    console.log(recentSearches);
+    var recentCity = "<h5>" + city + "</h5>";
+    if (recentSearches > 3) {
+        $("#recent-searches h5:last-child").remove();
+    }
+    $("#recent-searches").prepend(recentCity);
+}
 
+function updatePopularSearches() {
+    var numRef = firebase.database().ref('travelPlans');
+    numRef.orderByChild("numSearched").limitToLast(3).on("child_added", function (snapshot) {
+        var popular = snapshot.val().city;
+        var arr = popular.split(' ');
+        var result = "";
+        for (var i = 0; i < arr.length; i++) {
+            result += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1) + ' ';
+            var popularCap = (result.substring(0, result.length - 1));
+            var popularCity = "<h5 class='popular-city'>" + popularCap + "</h5>";
+            $("#popular-searches").append(popularCity);
+        }
+    });
+};
 
 $(document).ready(function () {
-    var apiKeyGetty = 'zd7c8j455yjfnjcmxvnhrdn8';
-    var gettyURL = "'https://api.gettyimages.com/v3/search/images?"
 
-    /*// Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyBNsIe37RELMNqecsYHHWmmhcw59jQkf88",
-        authDomain: "practicaltravel-63ff1.firebaseapp.com",
-        databaseURL: "https://practicaltravel-63ff1.firebaseio.com",
-        projectId: "practicaltravel-63ff1",
-        storageBucket: "",
-        messagingSenderId: "600055377899"
-    };
+    $('#submit-data').on('click', function (event) {
+        event.preventDefault();
 
-    var database = firebase.database();
-    // set a reference to the database service
-    firebase.initializeApp(config);
-    */
+        //OPEN WEATHER API
+        tempAndCoord();
 
-   $('#submit-data').on('click', function (event) {
-    event.preventDefault();
+        //GOOGLE SEARCH API CALL
+        googleSearchRest();
+        //GOOGLE HEALTH SEARCH API CALL
+        googleSearchHealth();
 
-    //OPEN WEATHER API
-    tempAndCoord();
+        //FIREBASE 
+        travelDatabase();
+        // reset search counters for each new input
+        numSearched = 1;
+        numSearchedCounter = 1;
 
-    //GOOGLE SEARCH API CALL
-    googleSearchRest();
-    //GOOGLE HEALTH SEARCH API CALL
-    googleSearchHealth();
+        // Flickr with slidesjs
+        flickrSlideshow();
 
-    //FIREBASE 
-    travelDatabase();
-    // reset search counters for each new input
-    numSearched = 1;
-    numSearchedCounter = 1;
+    });
 
-    // Flickr with slidesjs
-    flickrSlideshow();
 
-});
-
-    
 });
 
 
